d00d124223afa714c70973cf03ce3f8fccb63d9a
RJ Smith <randalljo@gmail.com>
2024-12-27 22:38:34 -0700

    plugins

--- lua/rj/lazy.lua
+++ lua/rj/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("rj.plugins")
+require("lazy").setup("rj.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
