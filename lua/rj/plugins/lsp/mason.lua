37e4de30200cbd927e81f3bf1d27cca5cc74ee48
RJ Smith <randalljo@gmail.com>
2024-12-29 14:51:51 -0700

    upadte lib

--- lua/rj/plugins/lsp/mason.lua
+++ lua/rj/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
+				"ts_ls",
 				"html",
 				"cssls",
 				"tailwindcss",
@@ -44,7 +44,6 @@ return {
 				"black", -- python formatter
 				"pylint",
 				"eslint_d",
-				"goimports",
 			},
 		})
 	end,
