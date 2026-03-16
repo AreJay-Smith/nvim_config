bcf7dc961e9834c0e8ea3e8f95da4da34aacfc18
RJ Smith <9436136+AreJay-Smith@users.noreply.github.com>
2026-03-16 11:13:19 -0600

    nvim pane size

--- lua/rj/core/keymaps.lua
+++ lua/rj/core/keymaps.lua
@@ -10,6 +10,12 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
+-- window resizing
+keymap.set("n", "<M-h>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap.set("n", "<M-l>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+keymap.set("n", "<M-j>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
+keymap.set("n", "<M-k>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
+
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width
